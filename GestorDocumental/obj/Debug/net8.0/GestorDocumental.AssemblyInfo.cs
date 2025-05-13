96556aac3872c88d8d57e3c9bd11c37ee2578baf
taniarb <tania.r.b.99@gmail.com>
2025-05-13 18:44:19 +0200

    validacion grupo

--- GestorDocumental/obj/Debug/net8.0/GestorDocumental.AssemblyInfo.cs
+++ GestorDocumental/obj/Debug/net8.0/GestorDocumental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorDocumental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af68ef94ad972523fdd1abcbba0000371d76f733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d28036937620a15663416c3cda3169fb92d4b3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorDocumental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorDocumental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
